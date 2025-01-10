afc7a1460d8e8f03803e44fc12d2ae1dab8e7cea
AbhinavJuyal <52655310+AbhinavJuyal@users.noreply.github.com>
2025-01-10 11:58:37 +0530

    feat: routing and tailwind setup done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { BrowseComponent } from './browse/browse.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'browse',
+    component: BrowseComponent,
+  },
+];
---
+++ src/app/browse/browse.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browse',
+  imports: [],
+  templateUrl: './browse.component.html',
+})
+export class BrowseComponent {}
